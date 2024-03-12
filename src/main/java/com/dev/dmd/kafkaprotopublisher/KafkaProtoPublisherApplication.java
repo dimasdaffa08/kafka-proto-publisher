fd9c21143249e57dbea4c06f83197d8d42c0e7f9
dimasdaffa08 <dimasdaffa9@gmail.com>
2024-03-12 09:38:46 +0700

    Initial Commit

---
+++ src/main/java/com/dev/dmd/kafkaprotopublisher/KafkaProtoPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.dev.dmd.kafkaprotopublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProtoPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProtoPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/dmd/kafkaprotopublisher/KafkaProtoPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.dmd.kafkaprotopublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProtoPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
